122355324e036646b91846037b80c4d38597dd83
2JooYeon <lewndus7749@naver.com>
2023-06-06 18:35:02 +0900

    Feat: 병합 비트 MIDI 파일 생성 및 업로드 기능 구현

--- app/utils.py
+++ app/utils.py
@@ -19,28 +19,30 @@ def s3_credential():
     return s3
 
 
-def match_instrument_sf_path():
-    sf_dir = "../soundfont"
-    match = {"piano": os.path.join(sf_dir, "GeneralUserMuseScore.sf2"), "bass": os.path.join(sf_dir, "JazzClubBass.sf2"), "drum" : os.path.join(sf_dir, "GeneralUserMuseScore.sf2")}
-    return match
-
-
-def stack_audio(waveforms):
-    stacked_waveform = np.zeros(np.max([w.shape[0] for w in waveforms]))
-    for waveform in waveforms:
-        stacked_waveform[:waveform.shape[0]] += waveform
-    stacked_waveform /= np.abs(stacked_waveform).max()
-    return stacked_waveform
-
-
 def download_sf(s3):
     sf_dir = "../soundfont"
     if not os.path.exists(sf_dir):
         os.makedirs(sf_dir)
     if not os.path.exists(os.path.join(sf_dir, "GeneralUserMuseScore.sf2")):
-        s3.download_file(bucket_name, "soundfont/GeneralUserMuseScore.sf2", os.path.join(sf_dir, "GeneralUserMuseScore.sf2"))
+        try:
+            s3.download_file(bucket_name, "soundfont/GeneralUserMuseScore.sf2", os.path.join(sf_dir, "GeneralUserMuseScore.sf2"))
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "404":
+                raise HTTPException(status_code=400,
+                                    detail="soundfont/GeneralUserMuseScore.sf2 key does not exist in the S3 bucket")
     if not os.path.exists(os.path.join(sf_dir, "JazzClubBass.sf2")):
-        s3.download_file(bucket_name, "soundfont/JazzClubBass.sf2", os.path.join(sf_dir, "JazzClubBass.sf2"))
+        try:
+            s3.download_file(bucket_name, "soundfont/JazzClubBass.sf2", os.path.join(sf_dir, "JazzClubBass.sf2"))
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "404":
+                raise HTTPException(status_code=400,
+                                    detail="soundfont/JazzClubBass.sf2 key does not exist in the S3 bucket")
+
+
+def match_instrument_sf_path():
+    sf_dir = "../soundfont"
+    match = {"piano": os.path.join(sf_dir, "GeneralUserMuseScore.sf2"), "bass": os.path.join(sf_dir, "JazzClubBass.sf2"), "drum" : os.path.join(sf_dir, "GeneralUserMuseScore.sf2")}
+    return match
 
 
 def download_voice(s3, filename):
@@ -67,7 +69,40 @@ def download_midi(s3, type, filename):
             raise HTTPException(status_code=400, detail=f"midi/{type}/{filename} key does not exist in the S3 bucket")
 
 
-def upload_stacked_audio(s3, filename, stacked_audio):
+def convert_beat(instrument_midi, sf_path):
+    instrument_beats = {}
+    for instrument, midi in instrument_midi.items():
+        beat = midi.fluidsynth(sf2_path=sf_path[instrument])
+        instrument_beats[instrument] = beat
+    return instrument_beats
+
+
+def upload_beat(s3, filename, instrument_beat):
+    for instrument, beat in instrument_beat.items():
+        write(f"{filename}.wav", 44100, beat)
+        ffmpeg.input(f"{filename}.wav").output(f"{filename}.m4a").run()
+        s3.upload_file(f"{filename}.m4a", bucket_name, f"beat/{instrument}/{filename}.m4a")
+        os.remove(f"{filename}.wav")
+        os.remove(f"{filename}.m4a")
+
+
+def upload_midi(s3, filename, instrument_midi):
+    for instrument, midi in instrument_midi.items():
+        midi_file = io.BytesIO()
+        midi.write(midi_file)
+        midi_file.seek(0)
+        s3.upload_fileobj(midi_file, bucket_name, f"midi/{instrument}/{filename}.mid")
+
+
+def stack_audio(waveforms):
+    stacked_waveform = np.zeros(np.max([w.shape[0] for w in waveforms]))
+    for waveform in waveforms:
+        stacked_waveform[:waveform.shape[0]] += waveform
+    stacked_waveform /= np.abs(stacked_waveform).max()
+    return stacked_waveform
+
+
+def upload_stacked_beat(s3, filename, stacked_audio):
     write(f"{filename}.wav", 44100, stacked_audio)
     ffmpeg.input(f"{filename}.wav").output(f"{filename}.m4a").run()
     s3.upload_file(f"{filename}.m4a", bucket_name, f"beat/stack/{filename}.m4a")
@@ -75,17 +110,8 @@ def upload_stacked_audio(s3, filename, stacked_audio):
     os.remove(f"{filename}.m4a")
 
 
-def upload_midi(s3, filename, instrument_midi, sf_path):
-    for instrument, midi in instrument_midi.items():
-        audio = midi.fluidsynth(sf2_path=sf_path[instrument])
-        # 추후에 파일 저장 없이 메모리 버퍼로만 진행할 수 있도록 코드 수정
-        write(f"{filename}.wav", 44100, audio)
-        ffmpeg.input(f"{filename}.wav").output(f"{filename}.m4a").run()
-        s3.upload_file(f"{filename}.m4a", bucket_name, f"beat/{instrument}/{filename}.m4a")
-        os.remove(f"{filename}.wav")
-        os.remove(f"{filename}.m4a")
-        # midi 파일 업로드
-        midi_file = io.BytesIO()
-        midi.write(midi_file)
-        midi_file.seek(0)
-        s3.upload_fileobj(midi_file, bucket_name, f"midi/{instrument}/{filename}.mid")
+def upload_stacked_midi(s3, filename, stacked_midi):
+    midi_file = io.BytesIO()
+    stacked_midi.write(midi_file)
+    midi_file.seek(0)
+    s3.upload_fileobj(midi_file, bucket_name, f"midi/stack/{filename}.mid")
