af6ac5619f567f83390a9afbcfb0174daec2b060
2JooYeon <lewndus7749@naver.com>
2023-06-06 18:34:30 +0900

    Feat: 병합 비트 MIDI 파일 생성 및 업로드 기능 구현

--- app/main.py
+++ app/main.py
@@ -11,30 +11,38 @@ s3 = s3_credential()
 download_sf(s3)
 sf_path = match_instrument_sf_path()
 
+
 class Voice(BaseModel):
     filename: str
+
 
 class Beat(BaseModel):
     filename: str
     type: str
 
+
 @app.post("/beats/convert", status_code=200)
 def voice2instrument(voice: Voice):
     y, sr = librosa.load(download_voice(s3, voice.filename))
     instrument_midi = voice2midi(y)
-    upload_midi(s3, voice.filename, instrument_midi, sf_path)
+    upload_midi(s3, voice.filename, instrument_midi)
+    instrument_beat = convert_beat(instrument_midi, sf_path)
+    upload_beat(s3, voice.filename, instrument_beat)
     return voice.filename
 
 
 @app.post("/beats/stack", status_code=200)
 def stack_beats(beats: List[Beat]):
     waveforms = []
-    stack_name = "voice_" + dt.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    stacked_name = "voice_" + dt.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    stacked_midi = pretty_midi.PrettyMIDI()
     for beat in beats:
-        # stack 타입 비트에 한번 더 병합을 시도할 경우 추가 예정
         midi = download_midi(s3, beat.type, beat.filename)
-        audio = midi.fluidsynth(sf2_path=sf_path[beat.type])
-        waveforms.append(audio)
+        for instrument in midi.instruments:
+            audio = instrument.fluidsynth(sf2_path=sf_path[instrument.name])
+            waveforms.append(audio)
+            stacked_midi.instruments.append(instrument)
     stacked_audio = stack_audio(waveforms)
-    upload_stacked_audio(s3, stack_name, stacked_audio)
-    return stack_name
+    upload_stacked_beat(s3, stacked_name, stacked_audio)
+    upload_stacked_midi(s3, stacked_name, stacked_midi)
+    return stacked_name
